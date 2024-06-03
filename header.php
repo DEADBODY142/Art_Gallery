6126166d198dd6e6e43df13a9ba9c25c973f0905
Kiran Shrestha <keyrunshrestha619@gmail.com>
2024-06-04 00:27:28 +0545

    initial attempt

---
+++ header.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Art Type</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
+
+</div>
