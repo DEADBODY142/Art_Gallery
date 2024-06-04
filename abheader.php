b59bdacac6317f1c6eda6a939721ff31d3be3576
Kiran Shrestha <keyrunshrestha619@gmail.com>
2024-06-04 15:55:04 +0545

    about_us

---
+++ abheader.php
@@ -0,0 +1,12 @@
+<nav>
+    <img src="" alt="" class="logo">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="aboutus.php">About Us</a></li>
+        <li><a href="">Art Type</a></li>
+        <li><a href="contactus.php">Contact Us</a></li>
+        <li><a href="../admin/login.php">Admin</a></li>
+    </ul>
+    <!-- <button class="btn">Bookings</button> -->
+</nav>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="">Home</a></li>
             <li><a href="">Gallery</a></li>
-            <li><a href="">About Us</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
             <li><a href="">Art Type</a></li>
             <li><a href="">Contact Us</a></li>
         </ul>
